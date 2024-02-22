ba3a5026e2d97fb7881da254e9f19453c6cab4be
Rafael Graça Silva <rg-silva@criticalsoftware.com>
2024-02-22 10:09:58 +0000

    cmake/configuration: look for CODAC quietly and print our own message after
    This is an effort to bring consistency to the find_package behaviour,
    Currently a message is always printed when CODAC is not found,
    this does not align with the common CMake pattern,
    additionally, it does not provide useful information to the user
    
    With this change a message is printed only on the first configuration,
    and then only when there are configuration changes

--- cmake/module/configuration.cmake
+++ cmake/module/configuration.cmake
@@ -2,12 +2,15 @@
 
 include(GNUInstallDirs)
 include(CTest)
+include(FindPackageMessage)
 
 # Detecting CODAC environment
 if(NOT COA_NO_CODAC)
-  find_package(CODAC OPTIONAL_COMPONENTS site-packages Python MODULE)
+  find_package(CODAC OPTIONAL_COMPONENTS site-packages Python MODULE QUIET)
 endif()
 if(CODAC_FOUND)
+  set(CODAC_FOUND_MESSAGE "Building with CODAC")
+
   # Append CODAC_CMAKE_PREFIXES to cmake seard directories, this helps cmake find packages installed in the CODAC enviorenment
   list(APPEND CMAKE_PREFIX_PATH ${CODAC_CMAKE_PREFIXES})
 
@@ -16,12 +19,22 @@ if(CODAC_FOUND)
     set(Python3_EXECUTABLE ${CODAC_PYTHON_EXECUTABLE})
   endif()
 
-  # When operating inside a CODAC CICD system build the documentation
+  # Check if operating inside a CODAC CICD system
   if(CODAC_CICD)
+    string(APPEND CODAC_FOUND_MESSAGE " CICD environment")
+
     set(COA_BUILD_DOCUMENTATION ON)
+  else()
+    string(APPEND CODAC_FOUND_MESSAGE " environment")
   endif()
+
+  find_package_message(
+    CODAC_DETAILS
+    "${CODAC_FOUND_MESSAGE}: ${CODAC_DIR} (version \"${CODAC_VERSION}\")"
+    "[${CODAC_FOUND}][${CODAC_DIR}][${CODAC_CICD}][v${CODAC_VERSION}]"
+  )
 else()
-  message(STATUS "Compiling without CODAC")
+  find_package_message(CODAC_DETAILS "Building without CODAC environment" "[${CODAC_FOUND}]")
 endif()
 
 get_filename_component(SUP_MATHEXPR_PROJECT_DIR "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
