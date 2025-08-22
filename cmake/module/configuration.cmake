c10cfdc7c651258a5123bfaf310bde0979122945
Walter Van Herck <walter.vanherck@iter.org>
2025-08-22 10:54:05 +0000

    Use common cmake file structure in SUP

--- cmake/module/configuration.cmake
+++ cmake/module/configuration.cmake
@@ -1,33 +1,6 @@
 # -----------------------------------------------------------------------------
-# Main project configuration
+# Common project configuration
 # -----------------------------------------------------------------------------
 
-include(GNUInstallDirs)
-include(CTest)
+include(COACommonConfig)
 include(COASetupCodacEnvironment)
-
-# -----------------------------------------------------------------------------
-# C++ version
-# -----------------------------------------------------------------------------
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "RelWithDebInfo")
-endif()
-
-# -----------------------------------------------------------------------------
-# Variables
-# -----------------------------------------------------------------------------
-set(LIBVERSION ${CMAKE_PROJECT_VERSION})
-set(LIBSOVERSION ${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR})
-
-# -----------------------------------------------------------------------------
-# Directories
-# -----------------------------------------------------------------------------
-if(NOT DEFINED TEST_OUTPUT_DIRECTORY)
-  set(TEST_OUTPUT_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_bin)
-endif()
-
-file(MAKE_DIRECTORY ${TEST_OUTPUT_DIRECTORY})
