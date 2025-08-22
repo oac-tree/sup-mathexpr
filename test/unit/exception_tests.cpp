85ecfea1788863d71c88dedd468c3846654744ee
Walter Van Herck <walter.vanherck@iter.org>
2025-08-22 11:51:52 +0000

    Add unit test for exceptions

---
+++ test/unit/exception_tests.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : SUP - MathExpr
+ *
+ * Description   : Mathematical expression evaluator for SUP
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2025 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ * SPDX-License-Identifier: MIT
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file LICENSE located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/mathexpr/exceptions.h>
+
+#include <gtest/gtest.h>
+
+using namespace sup::mathexpr;
+
+const std::string MESSAGE_1 = "message 1";
+
+class ExceptionTest : public ::testing::Test
+{
+protected:
+  ExceptionTest() = default;
+  virtual ~ExceptionTest() = default;
+};
+
+TEST_F(ExceptionTest, ExpressionEvaluateException)
+{
+  ExpressionEvaluateException exception{MESSAGE_1};
+  EXPECT_EQ(exception.what(), MESSAGE_1);
+}
